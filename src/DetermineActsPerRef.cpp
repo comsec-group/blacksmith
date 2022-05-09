3d437c967e6715d4a5705d13fc920dc2794afdc1
Luca Wilke <l.wilke@uni-luebeck.de>
2022-05-09 17:25:46 +0200

    use addr fns instead of timing to find conflicting addr

--- src/DetermineActsPerRef.cpp
+++ src/DetermineActsPerRef.cpp
@@ -26,15 +26,31 @@ int main(int argc, char** argv) {
   std::cerr << "Allocating memory...";
 
   Memory memory(true);
-  memory.allocate_memory(MEM_SIZE, false, false);
+  memory.allocate_memory(MEM_SIZE, false);
   memset((void*)memory.get_starting_address(), 0, MEM_SIZE);
-  DramAnalyzer dram_analyzer(memory.get_starting_address());
+  DramAnalyzer dram_analyzer(memory.get_starting_address(), memory.getSize());
+  Logger::log_info("loading dram config");
+  // initialize the DRAMAddr class to load the proper memory configuration
+  //luca initialize the global Config variable for memory with the data from the config file we edited
+  std::string identifier = "itsepyc2";
+  DRAMAddr::initialize(identifier, 2, memory.get_starting_address());
+
   std::cerr << "done\nDetermining bank conflicts...";
 
-  dram_analyzer.find_bank_conflicts();
+  //dram_analyzer.find_bank_conflicts();
+  auto addr1 = DRAMAddr((void*)(memory.get_starting_address()));
+  DRAMAddr addr2;
+  for( size_t offset = 0; offset < memory.getSize(); offset += 64) {
+    auto tmp = DRAMAddr((void*)(memory.get_starting_address()+offset));
+    if( tmp.bank == addr1.bank && tmp.row != addr1.row) {
+      addr2 = tmp;
+      break;
+    }
+  }
+  std::cerr << "choose " << addr1.to_string() << "and" << addr2.to_string() << "for row conflict addrs";
   std::cerr << "done\n Determining acts per ref...";
   for( int i = 0; i < 10;i++) {
-    std::cout << "\n" << dram_analyzer.count_acts_per_ref()*2 << "\n";
+    std::cout << "\n" << dram_analyzer.count_acts_per_ref((volatile char*)addr1.to_virt(),(volatile char*)addr2.to_virt())*2 << "\n";
   }
   std::cerr << "done\nThis is goodbye\n";
 }
--- src/DetermineConflictThresh.cpp
+++ src/DetermineConflictThresh.cpp
@@ -6,19 +6,65 @@
 
 #include<iostream>
 #include "Memory/Memory.hpp"
+#include "Memory/DramAnalyzer.hpp"
+
 
 int main() {
   Logger::initialize();
   std::cerr << "Allocating memory...";
 
   Memory memory(true);
-  memory.allocate_memory(MEM_SIZE, false, false);
+  memory.allocate_memory(MEM_SIZE, true);
   memset((void*)memory.get_starting_address(), 0, MEM_SIZE);
+  Logger::log_info("loading dram config");
+  // initialize the DRAMAddr class to load the proper memory configuration
+  //luca initialize the global Config variable for memory with the data from the config file we edited
+  std::string identifier = "itsepyc2";
+  GlobalConfig.setNumBanks(16);
+  DRAMAddr::initialize(identifier, 2, memory.get_starting_address());
 
-  std::cerr << "done\nMeasuring access timings using "<< GlobalConfig.getDramaRounds() <<" rounds per measurement ...";
+  const size_t sampleSize = 10000;
+  std::vector<std::tuple<uint64_t, uint64_t, uint64_t>> timings;
+  timings.reserve(3*sampleSize);
+  //
+  // Measure row hit timing
+  //
+  Logger::log_info("searching row hit entry...");
+  auto a1 = DRAMAddr((void*)memory.get_starting_address());
+  volatile char*  a1_row_hit;
+  for(uint8_t* ptr = (uint8_t*)memory.get_starting_address()+4096; ptr < ((uint8_t *)memory.get_starting_address())+memory.getSize(); ptr += 64) {
+    auto candidate_dram = DRAMAddr((void*)ptr);
+    if( a1.bank == candidate_dram.bank && a1.row == candidate_dram.row) {
+      a1_row_hit = (volatile char*)candidate_dram.to_virt();
+      break;
+    }
+  }
+  Logger::log_info("found same row entry. Measuring...");
 
-  DramAnalyzer dram_analyzer(memory.get_starting_address());
-  auto timings = dram_analyzer.measure_timings(10000);
+  for( size_t sampleIdx = 0; sampleIdx < 100000; sampleIdx++) {
+    auto timing = DramAnalyzer::measure_time((volatile char*)a1.to_virt(), a1_row_hit);
+    timings.emplace_back(std::make_tuple(reinterpret_cast<std::uintptr_t>(a1.to_virt()),reinterpret_cast<std::uintptr_t>(a1_row_hit),timing ));
+  }
+
+  //
+  //Measure row conflict timing
+  //
+
+  Logger::log_info("searching row conflict entry... ");
+  volatile char*  a1_row_conflict;
+  for(uint8_t* ptr = (uint8_t*)memory.get_starting_address()+4096; ptr < ((uint8_t *)memory.get_starting_address())+memory.getSize(); ptr += 64) {
+    auto candidate_dram = DRAMAddr((void*)ptr);
+    if( a1.bank == candidate_dram.bank && a1.row != candidate_dram.row) {
+      a1_row_conflict = (volatile char*)candidate_dram.to_virt();
+      break;
+    }
+  }
+  Logger::log_info("found row conflict entry. Measuring...");
+  for( size_t sampleIdx = 0; sampleIdx < 100000; sampleIdx++) {
+    auto timing = DramAnalyzer::measure_time((volatile char*)a1.to_virt(), a1_row_conflict);
+    timings.emplace_back(std::make_tuple(reinterpret_cast<std::uintptr_t>(a1.to_virt()),reinterpret_cast<std::uintptr_t>(a1_row_conflict),timing ));
+  }
+
 
   std::cerr << "done\nWriting csv file ...";;
 
