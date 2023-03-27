ea0ae6637d2b2522f353f306cb7efbe6f7cb9410
Jeremy Boy <github@jboy.eu>
2023-03-28 01:25:25 +0200

    style: auto-indent

--- include/Memory/DramAnalyzer.hpp
+++ include/Memory/DramAnalyzer.hpp
@@ -39,20 +39,20 @@ class DramAnalyzer {
     sum = 0;
 
     for (size_t i = 0; i < rounds; i++) {
-        mfence();
-        before = rdtscp();
-        *a1;
-        *a2;
-        after = rdtscp();
-        mfence();
-        delta = after-before;
-        if( delta < 200 || delta > 430 ) { //reject outliers
-			    i--; //if i =0; the i++ from the loop and will set it to 0 again, so no underflow
-	      } else {
-        	sum += delta;
-		    }
-        clflushopt(a1);
-        clflushopt(a2);
+      mfence();
+      before = rdtscp();
+      *a1;
+      *a2;
+      after = rdtscp();
+      mfence();
+      delta = after-before;
+      if( delta < 200 || delta > 430 ) { //reject outliers
+        i--; //if i =0; the i++ from the loop and will set it to 0 again, so no underflow
+      } else {
+        sum += delta;
+      }
+      clflushopt(a1);
+      clflushopt(a2);
     }
     return sum / rounds;
   }
