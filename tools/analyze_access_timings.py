0a963f36bd73b2835228947d9660fcfdc7950a67
Jeremy Boy <github@jboy.eu>
2023-03-29 12:29:23 +0200

    pass thresholds and CSV files as args to analyze_access_timings, update program output to improve machine readability

--- tools/analyze_access_timings.py
+++ tools/analyze_access_timings.py
@@ -1,29 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 def main():
-    files = ['access-timings.csv']
+    if len(sys.argv) < 4:
+        import os
+        print(f"Usage: {os.path.basename(sys.argv[0])} no-conflict-threshold conflict-threshold file1 [...file2]")
+        print(f"Example: {os.path.basename(sys.argv[0])} 250 300 access-timings.csv")
+        print("\tAnalyze access times from a CSV file, grouping access times in timings less than "
+              "no-conflict-threshold and greater than conflict-threshold.")
+        exit(255)
 
-    no_conflict_threshold = 300
-    conflict_threshold = 300
+    files = sys.argv[3:]
+
+    no_conflict_threshold = int(sys.argv[1])
+    conflict_threshold = int(sys.argv[2])
+
+    assert conflict_threshold >= no_conflict_threshold, "conflict threshold must be greater than or equal to " \
+                                                        "no-conflict-threshold "
 
     for f in files:
         data = pd.read_csv(f)
-        print(f)
-
-        print("Mean access time for non peak values {}".format(data[data['timing'] < no_conflict_threshold].mean()[0]))
+        lowTiming = data[data['timing'] < no_conflict_threshold]
+        print(f"len(<no-conflict-threshold): {lowTiming.count()[0]}")
+        print(f"mean(<no-conflict-threshold): {lowTiming.mean()[0]}")
         # build data frame that contains the indices from 'frame', where the
         # timing was over the specified threshold,  as values
 
         highTiming = data[data['timing'] > conflict_threshold]
         indicesHighTiming = pd.DataFrame(highTiming.index.array)
-        print("Got {} timing peaks".format(indicesHighTiming.count()[0]))
         diffBetweenHighTimings = highTiming.diff()
-        print("Mean access time for peak values {}. Difference between timing peaks is {} with std {}".format(
-            highTiming.mean()[0],
-            diffBetweenHighTimings.mean()[0],
-            diffBetweenHighTimings.std()[0]))
+        print(f"len(>conflict-threshold): {indicesHighTiming.count()[0]}")
+        print(f"mean(>conflict-threshold): {highTiming.mean()[0]}")
+        print(f"mean(diff(>conflict-threshold)): {diffBetweenHighTimings.mean()[0]}")
+        print(f"std(mean(diff(>conflict-threshold))): {diffBetweenHighTimings.std()[0]}")
 
         plt.show()
 
